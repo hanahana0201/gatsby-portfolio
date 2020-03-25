d7acb6a8a9ee9d4b8c645201f6c23e17a3047eee
Masatoshi HANAI <hanahana0201@gmail.com>
2020-03-25 21:48:43 +0900

    ReactComponent からデータを呼び出す

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`,`application`,`react`]} />
     <h1>Hello Great Gatsby</h1>
-    <Link to="/about/">Go to about page</Link>
+    {data.allWorksYaml.edges.map(edge => {
+      const work = edge.node
+      return <div>
+        {work.title} - {work.category} - {work.year}
+      </div>
+    })}
+    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export  const query = graphql`
+  query {
+    allWorksYaml {
+      edges {
+        node {
+          title
+          category
+          year
+        }
+      }
+    }
+  }      
+`
 
 export default IndexPage
